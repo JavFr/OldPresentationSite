867f0a7241d8e8861263c76ae7ebc222c7abee0e
JavFr <jfbrustolon@gmail.com>
2019-07-18 12:16:50 -0300

    Escape and Enter Key interactions, and Montserrat Font

--- js/main.js
+++ js/main.js
@@ -1,64 +1,69 @@
 $(document).ready(function(){
-    /*
-    $("#complete-cube").click(function(){
-        $("#complete-cube").css({'visibility': "hidden"})
-        $("#cube-2").removeClass('all-as-cube-1');
-        $("#cube-3").removeClass('all-as-cube-1');
-        $("#cube-4").removeClass('all-as-cube-1');
-        $("#cube-5").removeClass('all-as-cube-1');
-        $("#cube-6").removeClass('all-as-cube-1');
-        $('main').css({'grid-gap': '20px'});
-    });
+
+    /*Open Cube info when it's click*/
     $("#cube-1").click(function(){      
-        $("#cube-2").addClass('all-as-cube-1');
-        $("#cube-3").addClass('all-as-cube-1');
-        $("#cube-4").addClass('all-as-cube-1');
-        $("#cube-5").addClass('all-as-cube-1');
-        $("#cube-6").addClass('all-as-cube-1');
-        $('main').css({'grid-gap': '0'});
-        $("#complete-cube").css({"background": "#ff595e", 'visibility': "visible"})
+        toogleCube("cube-1", "#ff595e");
     });
     $("#cube-2").click(function(){      
-        $("#cube-1").addClass('all-as-cube-2');
-        $("#cube-3").addClass('all-as-cube-2');
-        $("#cube-4").addClass('all-as-cube-2');
-        $("#cube-5").addClass('all-as-cube-2');
-        $("#cube-6").addClass('all-as-cube-2');
-        $('main').css({'grid-gap': '0'});
-        $("#complete-cube").css({"background": "#ffca3a", 'visibility': "visible"})
+        toogleCube("cube-2", "#ffca3a");
     });
-    */
-   $("#cube-1").click(function(){      
-    $('main').css({'grid-gap': '0'});
-    $("#complete-cube").css({"background": "#ff595e", 'z-index': "999"});
-    $("#info-cube-1").addClass('show-info').removeClass('hide-info');
-   });
-   $("#cube-2").click(function(){      
-    $('main').css({'grid-gap': '0'});
-    $("#complete-cube").css({"background": "#ffca3a", 'z-index': "999"});
-    $("#info-cube-2").addClass('show-info').removeClass('hide-info');
-   });
-   $("#cube-3").click(function(){      
-    $('main').css({'grid-gap': '0'});
-    $("#complete-cube").css({"background": "#06d6a0", 'z-index': "999"});
-    $("#info-cube-3").addClass('show-info').removeClass('hide-info');
-   });
-   $("#cube-4").click(function(){      
-    $('main').css({'grid-gap': '0'});
-    $("#complete-cube").css({"background": "#1982c4", 'z-index': "999"});
-    $("#info-cube-4").addClass('show-info').removeClass('hide-info');
-   });
-   $("#cube-5").click(function(){      
-    $('main').css({'grid-gap': '0'});
-    $("#complete-cube").css({"background": "#6a4c93", 'z-index': "999"});
-    $("#info-cube-5").addClass('show-info').removeClass('hide-info');
-   });
-   $("#cube-6").click(function(){      
-    $('main').css({'grid-gap': '0'});
-    $("#complete-cube").css({"background": "#8ac926", 'z-index': "999"});
-    $("#info-cube-6").addClass('show-info').removeClass('hide-info');
-   });
-    $("#complete-cube").click(function(){
+    $("#cube-3").click(function(){      
+        toogleCube("cube-3", "#06d6a0");
+    });
+    $("#cube-4").click(function(){      
+        toogleCube("cube-4", "#1982c4");
+    });
+    $("#cube-5").click(function(){      
+        toogleCube("cube-5", "#6a4c93");
+    });
+    $("#cube-6").click(function(){      
+        toogleCube("cube-4", "#8ac926");
+    });
+    /* Hide the cube info whe is click, or when escape key is pressed*/
+    $("#complete-cube").click(function(){hideCompleteCube()});
+
+    $(document).on('keydown', function(event) {
+        if (event.key == "Escape") {
+            hideCompleteCube();
+        }
+    });
+
+    /*Allow to open Cubes with Enter Key*/
+    $("#cube-1").on('keydown', function(event) {
+        if (event.key == "Enter") {
+            toogleCube("cube-1", "#ff595e");
+        }
+    });
+    $("#cube-2").on('keydown', function(event) {
+        if (event.key == "Enter") {
+            toogleCube("cube-2", "#ffca3a");
+        }
+    });
+    $("#cube-3").on('keydown', function(event) {
+        if (event.key == "Enter") {
+            toogleCube("cube-3", "#06d6a0");
+        }
+    });
+    $("#cube-4").on('keydown', function(event) {
+        if (event.key == "Enter") {
+            toogleCube("cube-4", "#1982c4");
+        }
+    });
+    $("#cube-5").on('keydown', function(event) {
+        if (event.key == "Enter") {
+            toogleCube("cube-5", "#6a4c93");
+        }
+    });
+    $("#cube-6").on('keydown', function(event) {
+        if (event.key == "Enter") {
+            toogleCube("cube-4", "#8ac926");
+        }
+    });
+    /*                  */
+
+});
+
+function hideCompleteCube (){
         $("#info-cube-1").addClass('hide-info').removeClass('show-info');
         $("#info-cube-2").addClass('hide-info').removeClass('show-info');
         $("#info-cube-3").addClass('hide-info').removeClass('show-info');
@@ -67,6 +72,10 @@ $(document).ready(function(){
         $("#info-cube-6").addClass('hide-info').removeClass('show-info');
         $("#complete-cube").css({'z-index': "-1", "background": "transparent"})
         $('main').css({'grid-gap': '15px'});
-    });
-});
+}
 
+function toogleCube(cube, background){
+    $('main').css({'grid-gap': '0'});
+    $("#complete-cube").css({"background": background, 'z-index': "999"});
+    $("#info-"+cube).addClass('show-info').removeClass('hide-info');
+}
